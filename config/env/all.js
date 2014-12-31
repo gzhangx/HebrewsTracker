7494299d5eb6a67edf7b9d4f5ea65d5a179c5e84
Gang Zhang <gang.zhang@pps.io>
2014-12-31 01:12:53 -0500

    fix display

--- config/env/all.js
+++ config/env/all.js
@@ -2,15 +2,15 @@
 
 module.exports = {
 	app: {
-		title: 'MEAN.JS',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
+		title: 'Hebrews',
+		description: 'Hebrews mean bible tracker',
+		keywords: 'Hebrews Bible Tracker'
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
 	// The secret should be set to a non-guessable string that
 	// is used to compute a session hash
-	sessionSecret: 'MEAN',
+	sessionSecret: 'hebrewsGogoHspirit',
 	// The name of the MongoDB collection to store sessions in
 	sessionCollection: 'sessions',
 	// The session cookie settings
