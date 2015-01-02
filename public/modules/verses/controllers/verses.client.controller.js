a5e4b3779e38e02776d6a0c7f74c93f8bced0787
Gang Zhang <gang.zhang@pps.io>
2015-01-02 01:02:46 -0500

    cookie support

--- public/modules/verses/controllers/verses.client.controller.js
+++ public/modules/verses/controllers/verses.client.controller.js
@@ -6,19 +6,19 @@ angular.module('verses').controller('VersesController', ['$scope', '$stateParams
         function setCookie(cname, cvalue, exdays) {
             var d = new Date();
             d.setTime(d.getTime() + (exdays*24*60*60*1000));
-            var expires = "expires="+d.toUTCString();
-            document.cookie = cname + "=" + cvalue + "; " + expires;
+            var expires = 'expires='+d.toUTCString();
+            document.cookie = cname + '=' + cvalue + '; ' + expires;
         }
 
         function getCookie(cname) {
-            var name = cname + "=";
+            var name = cname + '=';
             var ca = document.cookie.split(';');
             for(var i=0; i<ca.length; i++) {
                 var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1);
-                if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+                while (c.charAt(0)===' ') c = c.substring(1);
+                if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
             }
-            return "";
+            return '';
         }
 
 		$scope.authentication = Authentication;
