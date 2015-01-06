444d54b1d5f663afb3be956073f19598165e2eaa
Gang Zhang <gang.zhang@pps.io>
2015-01-05 21:46:36 -0500

    fix warning

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -12,7 +12,8 @@ var _ = require('lodash'),
 exports.validateEmail = function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-}
+};
+
 /**
  * Signup
  */
