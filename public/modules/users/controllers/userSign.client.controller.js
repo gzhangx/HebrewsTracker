2d732e3f689efa36fd244165d56895ca4fa90402
Gang Zhang <gang.zhang@pps.io>
2015-01-15 16:44:01 -0500

    refac

--- public/modules/users/controllers/userSign.client.controller.js
+++ public/modules/users/controllers/userSign.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('users').controller('UserInfoController', ['$scope', 'Authentication','VersesDirect',
+angular.module('users').controller('UserSignController', ['$scope', 'Authentication','VersesDirect',
 	function($scope, Authentication, VersesDirect) {
 		$scope.authentication = Authentication;
 
