e7f89f11df1697067fc51b2abef415caccecc514
Gang Zhang <gang.zhang@pps.io>
2015-01-16 11:02:51 -0500

    starting sign route

--- public/modules/users/controllers/userSign.client.controller.js
+++ public/modules/users/controllers/userSign.client.controller.js
@@ -4,10 +4,27 @@ angular.module('users').controller('UserSignController', ['$scope', 'Authenticat
 	function($scope, Authentication, VersesDirect) {
 		$scope.authentication = Authentication;
 
+        $scope.recordedHash = {};
+        $scope.curSchedule = [];
+        $scope.scheduleStarts = [];
+        $scope.scheduleStartSel ={};
+        $scope.allStats = [];
 
+        VersesDirect.scheduleDctf($scope.authentication.user.email, function(){
+            $scope.curSchedule = VersesDirect.curSchedule;
+            $scope.allStats = VersesDirect.allStats;
+            $scope.recordedHash = VersesDirect.recordedHash;
+            var scheduleStarts = VersesDirect.scheduleStarts;
+            $scope.scheduleStarts = scheduleStarts;
+            if (scheduleStarts.length > 0) {
+                $scope.scheduleStartSel = scheduleStarts[0];
+            }
+        });
+
+        $scope.scheduleChanged = function() {};
         $scope.RequestSign = function(){
-            //VersesDirect.
-        }
+
+        };
 
 	}
 ]);
